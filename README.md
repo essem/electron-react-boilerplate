ef0a614f681a7b615baaf063def98001bbdf5e62
essem <soomin@gmail.com>
2016-01-10 17:35:54 +0900

    Convert project for electron.

--- README.md
+++ README.md
@@ -1,32 +1,28 @@
-# webpack-express-boilerplate
-A boilerplate for running a Webpack workflow in Node express
+# electron-react-boilerplate
 
-Please read the following article: [The ultimate Webpack setup](http://www.christianalfoni.com/articles/2015_04_19_The-ultimate-webpack-setup) to know more about this boilerplate.
+A boilerplate for building a React-Electron app.
+Converted from  [webpack-express-boilerplate](
+https://github.com/christianalfoni/webpack-express-boilerplate)
 
-## Major update to project
-Inspired by [this project](https://github.com/vesparny/react-kickstart) and the evolving of [react-transform](https://github.com/gaearon/react-transform-boilerplate) and [CSS Modules]((http://glenmaddern.com/articles/css-modules)), this project has gotten a major upgrade.
+## Run
 
-**NOTE!** Use the latest version of Node, 4.x.x.
+Install node modules.
 
-## Install
-`git clone https://github.com/christianalfoni/webpack-express-boilerplate.git`
+Example with docker.
 
-or just export the files:
+```bash
+docker run -it --rm -v ${PWD}:/src node:5.1.1 sh -c "cd /src && npm install"
+```
+For production mode, build bundle.
 
-`svn export https://github.com/christianalfoni/webpack-express-boilerplate/trunk ./dir`
+Example with docker.
 
-## Overview
+```bash
+docker run -it --rm -v ${PWD}:/src -e NODE_ENV=production node:5.1.1 sh -c "cd /src && npm run build"
+```
 
-### React by default
-The project runs with React by default and hot replacement of changes to the modules. Currently it is on 0.14.3.
+Run with electron in project root directory.
 
-### CSS Modules
-CSS files loaded into components are locally scoped and you can point to class names with javascript. You can also compose classes together, also from other files. These are also hot loaded. Read more about them [here](http://glenmaddern.com/articles/css-modules).
-
-To turn off CSS Modules remove it from the `webpack.config.js` file.
-
-### Babel and Linting
-Both Node server and frontend code runs with Babel. And all of it is linted. With atom you install the `linter` package, then `linter-eslint` and `linter-jscs`. You are covered. Also run `npm run eslint` or `npm run jscs` to verify all files. I would recommend installing `language-babel` package too for syntax highlighting
-
-### Beautify
-With a beautify package installed in your editor it will also do that
+```bash
+${ELECTRON_PATH}/electron .
+```
---
+++ main.js
@@ -0,0 +1,95 @@
+'use strict';
+
+let startUrl = 'file://' + __dirname + '/dist/index.html';
+const isDeveloping = process.env.NODE_ENV !== 'production';
+
+if (isDeveloping) {
+  const path = require('path');
+  const express = require('express');
+  const webpack = require('webpack');
+  const webpackMiddleware = require('webpack-dev-middleware');
+  const webpackHotMiddleware = require('webpack-hot-middleware');
+  const config = require('./webpack.config.js');
+
+  const app = express();
+  const compiler = webpack(config);
+  const middleware = webpackMiddleware(compiler, {
+    publicPath: config.output.publicPath,
+    contentBase: 'src',
+    stats: {
+      colors: true,
+      hash: false,
+      timings: true,
+      chunks: false,
+      chunkModules: false,
+      modules: false
+    }
+  });
+
+  app.use(middleware);
+  app.use(webpackHotMiddleware(compiler));
+  app.get('*', function response(req, res) {
+    res.write(middleware.fileSystem.readFileSync(path.join(__dirname, 'dist/index.html')));
+    res.end();
+  });
+
+  const port = 9865;
+  startUrl = `http://0.0.0.0:${port}`;
+  app.listen(port, '0.0.0.0', function onStart(err) {
+    if (err) {
+      console.log(err);
+    }
+    console.info('==> 🌎 Listening on port %s. Open up %s in your browser.', port, startUrl);
+  });
+}
+console.log('Starting URL: ' + startUrl);
+
+const electron = require('electron');
+// Module to control application life.
+const app = electron.app;
+// Module to create native browser window.
+const BrowserWindow = electron.BrowserWindow;
+
+// Keep a global reference of the window object, if you don't, the window will
+// be closed automatically when the JavaScript object is garbage collected.
+let mainWindow;
+
+function createWindow() {
+  // Create the browser window.
+  mainWindow = new BrowserWindow({width: 800, height: 600});
+
+  // and load the index.html of the app.
+  mainWindow.loadURL(startUrl);
+
+  // Open the DevTools.
+  mainWindow.webContents.openDevTools();
+
+  // Emitted when the window is closed.
+  mainWindow.on('closed', function() {
+    // Dereference the window object, usually you would store windows
+    // in an array if your app supports multi windows, this is the time
+    // when you should delete the corresponding element.
+    mainWindow = null;
+  });
+}
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+app.on('ready', createWindow);
+
+// Quit when all windows are closed.
+app.on('window-all-closed', function() {
+  // On OS X it is common for applications and their menu bar
+  // to stay active until the user quits explicitly with Cmd + Q
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', function() {
+  // On OS X it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (mainWindow === null) {
+    createWindow();
+  }
+});
